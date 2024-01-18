9fa517f7b37901d5e6dfdc241cd03d18291c78ab
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-01-18 12:12:24 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3); // code readability should br high
+
+    console.log("Akkya");
+
+    let name = "Akkya";
+    let age = 22;
+
+    /* Preemitive datatypes -
+    // number => 2 to the power 53
+    // bigint
+    // string => '' or "" 
+    // boolean => true/false
+    // null => standalone value / value is empty (type : object)
+    // undefined
+    // symbol => uniqueness
+    */
+
+    // object
+
+console.log(typeof "abc"); //string
+console.log(typeof 123); //number
+console.log(typeof age); //number
+console.log(typeof name); //string
+
+console.log(typeof undefined) //undefined
+console.log(typeof null); //object
