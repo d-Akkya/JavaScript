483e2be07fbd84b8d28a14bdd01527a7962f3315
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-01-19 18:35:45 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Akkya";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// string => NaN
+// undefined => NaN
+// null => 0
+
+
+let isLoggedIn = "Akkya";
+let booleanValue = Boolean(isLoggedIn);
+console.log(booleanValue);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akkya" => true
+
+let stringNumber = 33;
+let stringValue = String(stringNumber)
+console.log(stringValue);
+console.log(typeof (stringValue));
+
