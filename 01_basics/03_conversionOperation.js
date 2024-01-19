e2ab956bda1b7defedb9994ec7a030831c14b759
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-01-19 19:02:19 +0000

    Discussed some operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,4 @@
+/*
 let score = "Akkya";
 
 console.log(typeof score);
@@ -6,6 +7,7 @@ console.log(typeof (score));
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+*/
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,16 +17,45 @@ console.log(valueInNumber);
 // null => 0
 
 
+/*
 let isLoggedIn = "Akkya";
 let booleanValue = Boolean(isLoggedIn);
 console.log(booleanValue);
+*/
 
 // 1 => true; 0 => false
 // "" => false
 // "Akkya" => true
 
+/* 
 let stringNumber = 33;
 let stringValue = String(stringNumber)
 console.log(stringValue);
 console.log(typeof (stringValue));
+*/
+
+// ******************* Operations ***********************
+
+/* let value = 3;
+let negValue = -value;
+console.log(negValue);
+*/ 
+
+ /* console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3); */
+
+let str1 = "Hello";
+let str2 = " Akkya";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+
+
+
+
 
