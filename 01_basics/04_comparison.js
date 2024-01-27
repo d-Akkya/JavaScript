618897fd5fac3aabce19c109f1790f67936d1181
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-01-27 14:10:51 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 > 2);
+console.log(1 <= 2);
+console.log(2 == 3);
+console.log(1 != 2);
+
+console.log("2" > 1);
+console.log("02" < 1); //this type of comparison is possible but always check weither datatypes are same for confusion.
+
+console.log(null == 0);
+console.log();
+
+
+
+
+
+
