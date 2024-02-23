9e2cdd15667a914511f55718cb7109ea8ecf2d96
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-02-23 12:20:52 +0000

    Array part-1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -23,3 +23,31 @@ console.log(myArr2.includes(9)); // we asking question to array by includes(), i
 
 console.log(myArr2.indexOf(3)); // we can check the index of following element => 2
 console.log(myArr2.indexOf(19)); // but we check the index of non-existing value it return -1
+
+const newArr = myArr2.join() // join operator converts/adds all the elements of an array into a string
+console.log(newArr); // typeof - string data type
+
+//slice, splice methods:
+console.log(myArr2.slice(1, 3)); //in this method we need to declare start and end range so it will return the range elements, but end range is not included => [2, 3]
+console.log(myArr2); // so here you can see the main values are not changed and not manipulated by slice() function => [1, 2, 3, 4]
+
+myArr2.push(5) // adding 5 to the array, so the length of array is 5
+
+console.log(myArr2.splice(1, 3)); // in this method also we need to declare start & end range, so it will return the range elements, end range is included in this method => [2, 3, 4]
+console.log(myArr2); //here is main changes happens in array. The splice() function will return the passed range elements, but it manipulates the main array values & array will have the remaining elements after the function range => [1, 5] 
+//Another example-
+let array = [3, 6, 8, 9, 7, 5, 2]
+console.log(`A: ${array}`);
+
+const myn1 = array.slice(2, 5);
+console.log(`B: ${myn1}`);
+
+const myn2 = array.splice(1, 5)
+console.log(`C: ${myn2}`);
+console.log(array);
+
+/*
+    The main difference between slice & splice in array:
+1) Slice will return the range elements but not include the end index, whereas Splice will return the range elements including end index.
+2) Slice will return the elements but it didn't make changes in main array, whereas Splice will return the range elements but it manipulates the main array values.
+*/
