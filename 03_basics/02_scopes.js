e38d9451e99fd2460cf83abade8d12b64154ea5d
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-03-06 02:31:33 +0530

    block & global scope discussion

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,7 +1,18 @@
-let a = 10
-const b = 20
-var c = 30
+// Scopes:-
+// In simple words {} is scope except Object
 
-console.log(a);
-console.log(b);
-console.log(c);
+// var c = 400
+let a = 300
+
+if (true) {
+    let a = 10
+    const b = 20
+    c = 30 // not only var, but variable without any keyword will also give same problem
+    console.log("Inner: ", a);
+}
+
+console.log(a); //if not declared in global scope but declared in block scope and print the value in global scope then it will show error 'undefined'.
+// console.log(b); //error
+console.log(c); //30 (further will face issues)
+
+// There's difference between windows (browser) global scope & node (code run through node) global scope.
