d8c0fbfbd7ff02d151f1be847467ec7abc9f8804
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-01 20:24:09 +0530

    nesting

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -58,6 +58,6 @@ if (currentBalance < 500) {
 const userLoggedIn = true
 const debitCard = true
 
-if (userLoggedIn && debitCard) {
+if (userLoggedIn && debitCard && 2==2) {
     console.log("Allow to buy course");
 }
