ea7e92712b7e174b72903be283169b2e1a876ed8
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-03 12:08:20 +0530

    switch case statement

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,11 +1,32 @@
 // switch-case statement
 //it is used in Redux.
 
-switch (key) {
-    case value:
+// switch (key) {
+//     case value:
         
+//         break;
+
+//     default:
+//         break;
+// } //basic syntax
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
         break;
 
     default:
+        console.log("Out of reach");
         break;
 }
