e183aa2dd131cfdad2e029478aec97de7132aa70
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-08 03:27:31 +0000

    for loop

--- 05_iterations_(loops)/one.js
+++
@@ -1,9 +0,0 @@
-// for
-
-for (let i = 0; i <= 10; i++) {
-    const element = i
-    console.log(element);
-    if (element == 5) {
-        console.log("5 is best number");
-    }
-}
---
+++ 05_iterations_loops/one.js
@@ -0,0 +1,9 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i
+    if (element == 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+}   //rule first we initialize variable, second condition if it's true (eg. if) then loop is executed till the second-last line of the scope of loop and returns to the third stage i.e i++ till the second condition satisfies. 
