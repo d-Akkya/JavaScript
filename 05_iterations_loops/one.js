d3ffa71bcc6071b262ee4234bd64258bde3d0f20
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-15 08:50:36 +0000

    for-loop on array

--- 05_iterations_loops/one.js
+++ 05_iterations_loops/one.js
@@ -3,18 +3,28 @@
 for (let i = 0; i <= 10; i++) {
     const element = i
     if (element == 5) {
-        console.log("5 is best number");
+        //console.log("5 is best number");
     }
-    console.log(element);
+    //console.log(element);
 }   //rule first we initialize variable, second condition if it's true (eg. if) then loop is executed till the second-last line of the scope of loop and returns to the third stage i.e i++ till the second condition satisfies. 
+
 
 //Let's use loop under loop:
 for (let i = 1; i <= 10; i++) //we can use 'i' as variable because previously used 'i' is limited for that respective loop (block scope scope)
 {
-    console.log(`Outer loop value: ${i}`);
+    //console.log(`Outer loop value: ${i}`);
     for (let j = 1; j <= 10; j++) //here we can't use variable as 'i' because this loop is under a loop, so it creates a mismatch problem.
     {
         // console.log(`Inner loop value: ${j} and outer loop is ${i}`);
-        console.log(`${i} * ${j} = ${i*j}`);                
+        //console.log(`${i} * ${j} = ${i*j}`);                
     }    
+}
+
+
+//Basic syntax of for-loop is on array:
+const myArray = ["Batman", "Superman", "Flash"]
+console.log(myArray.length); //3
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
 }
