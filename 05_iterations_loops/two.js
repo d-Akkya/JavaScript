217d8b6fafe5677046879f63335e01cbf22a8d7c
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-17 09:19:15 +0000

    while on array

--- 05_iterations_loops/two.js
+++ 05_iterations_loops/two.js
@@ -5,3 +5,13 @@ while (index <= 10) {
     console.log(index);
     index = index + 2
 }
+
+// while-loop on array:
+let myArray = ["Batman", "Superman", "Flash"]
+let arr = 0
+
+while (arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+Object.keys(myObj);
