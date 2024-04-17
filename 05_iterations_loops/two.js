52230bee59f9d7d98f4730e2928fc81a46c2b101
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-17 09:43:46 +0000

    do-while loop

--- 05_iterations_loops/two.js
+++ 05_iterations_loops/two.js
@@ -1,10 +1,10 @@
 // while loop
-
 let index = 0;
 while (index <= 10) {
     console.log(index);
     index = index + 2
 }
+
 
 // while-loop on array:
 let myArray = ["Batman", "Superman", "Flash"]
@@ -14,3 +14,11 @@ while (arr < myArray.length){
     console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
+
+
+//do-while loop:
+let gameScore = 1
+do {
+    console.log(`Score is ${gameScore}`);
+    gameScore++
+} while (gameScore <= 10);
