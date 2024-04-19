385c6873065d45d18ec75f2f6fefe1082786a093
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-19 08:51:31 +0000

    Map in js

--- 05_iterations_loops/three.js
+++ 05_iterations_loops/three.js
@@ -24,5 +24,6 @@ const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
+map.set('IN', "India") //this will not print because map contains only unique key-values.
 
 console.log(map);
