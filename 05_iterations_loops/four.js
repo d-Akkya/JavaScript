7aeb2cc57d75c6cff25878126b8ed36a0925584c
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-21 08:18:52 +0000

    for-of loop on array

--- 05_iterations_loops/four.js
+++ 05_iterations_loops/four.js
@@ -9,3 +9,13 @@ const myObject = {
 for (const key in myObject) {
     console.log(`${key} shortcut is for ${myObject[key]}`)
 }
+
+
+// for-in on array:
+const programming = ['js', "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key); //but it's printing only keys of array in this loop, as we see the for-of loop gives the values of array. Yes it is the major syntax difference between for-of & for-in loop.
+    console.log(programming[key]);  
+}
+//Array also have keys by default numbers starts with 0, but in object we can add keys whatever we want.
