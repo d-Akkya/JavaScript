24fd4df13e34f6d0f89e8b0ad73cb6afa69a9016
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-21 07:54:47 +0000

    created an object

--- 05_iterations_loops/four.js
+++ 05_iterations_loops/four.js
@@ -1,5 +1,11 @@
 // for-in loop :-
+const myObject = {
+    js: "javascript",
+    rb: "ruby",
+    cpp: "C++",
+    swift: "swift by apple"
+}
 
-for (const key in object) {
+for (const key in Object) {
     
 }
