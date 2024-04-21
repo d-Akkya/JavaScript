855285feeabd92c2841db127d528dcc28cb4189f
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-21 09:26:14 +0000

    ways of functions to for-each

--- 05_iterations_loops/five.js
+++ 05_iterations_loops/five.js
@@ -4,3 +4,12 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 coding.forEach( function (item) {
     console.log(item)
 } ) //we simply declared a function without name, because it is callback function.
+
+coding.forEach( (i) => {
+    console.log(i)
+} )
+
+const greet = function (index) {
+    console.log(index);
+}
+coding.forEach(greet)
