0a80b8b89b6706cfcb01d0e791c0d7b6a9d7d458
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-21 14:01:48 +0000

    forEach with variations

--- 05_iterations_loops/five.js
+++ 05_iterations_loops/five.js
@@ -1,5 +1,6 @@
-// for-each loop :-
+// forEach loop :-
 const coding = ["js", "ruby", "java", "python", "cpp"]
+//forEach loop is directly added to properties of array. It id a higher order function.
 
 coding.forEach( function (item) {
     console.log(item)
@@ -7,9 +8,12 @@ coding.forEach( function (item) {
 
 coding.forEach( (i) => {
     console.log(i)
-} )
+} ) //anpther variation by arrow fn
 
 const greet = function (index) {
     console.log(index);
 }
-coding.forEach(greet)
+coding.forEach(greet) //another variation by injecting/reference of fn.
+//Remember giving a reference of function not executing (), printing job will done automatically.
+
+
