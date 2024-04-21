c0a994ddc7894d310d4e0ac73b6296b92b0198c4
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-21 09:22:02 +0000

    for-each with basic function syntax

--- 05_iterations_loops/five.js
+++ 05_iterations_loops/five.js
@@ -1,1 +1,6 @@
 // for-each loop :-
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function (item) {
+    console.log(item)
+} ) //we simply declared a function without name, because it is callback function.
