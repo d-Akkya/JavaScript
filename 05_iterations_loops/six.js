0198547ff1fa875c66811613cfb2d62dca7c7643
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-22 17:57:08 +0000

    creating a array for filter

--- 05_iterations_loops/six.js
+++ 05_iterations_loops/six.js
@@ -9,7 +9,7 @@ console.log(values); //forEach is not returning any value
 //Sometimes we get the situation when we don't need the values of array must be printed, we need to return or get the values from array on the condition basis.
 
 //for that situation we use filter:
-const num = 
-coding.filter(  )
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 
