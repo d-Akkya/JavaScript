5fae46dda2c90c3d0a642b7e08316b89dc37664d
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-22 17:36:23 +0000

    forEach discussion

--- 05_iterations_loops/six.js
+++ 05_iterations_loops/six.js
@@ -2,8 +2,10 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 
 const values = coding.forEach( (item) => {
     console.log(item)
-    return item
+    return item //return not works in forEach
 } )
 
 console.log(values); //forEach is not returning any value
 //Sometimes we get the situation when we don't need the values of array must be printed, we need to return or get the values from array on the condition basis.
+
+
