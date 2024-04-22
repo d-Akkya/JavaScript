9ebbf7ebba28c95b220cd7bf3613c4fa6e38e72a
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-22 17:53:32 +0000

    filter

--- 05_iterations_loops/six.js
+++ 05_iterations_loops/six.js
@@ -8,4 +8,8 @@ const values = coding.forEach( (item) => {
 console.log(values); //forEach is not returning any value
 //Sometimes we get the situation when we don't need the values of array must be printed, we need to return or get the values from array on the condition basis.
 
+//for that situation we use filter:
+const num = 
+coding.filter(  )
+
 
