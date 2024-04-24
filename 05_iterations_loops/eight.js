b6ecfcc18bffb72eb8d02bb6b95d87ae3a1b4ffa
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-24 12:35:17 +0000

    reduce function

--- 05_iterations_loops/eight.js
+++ 05_iterations_loops/eight.js
@@ -1,1 +1,11 @@
 // reduce method:-
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce( function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0  )        normal function
+
+const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0 ) //arrow function
+
+console.log(myTotal);
