c0f621f25c0dc8bac01b0efc521a724cf8e843bf
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-24 12:44:14 +0000

    reduce loop/function

--- 05_iterations_loops/eight.js
+++ 05_iterations_loops/eight.js
@@ -11,5 +11,26 @@ const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0 ) //arrow functi
 console.log(myTotal);
 
 //Realtime example of reduce:
-const shoppingCart
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course ",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0 )
+
+console.log(priceToPay);
 
