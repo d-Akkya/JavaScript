3b670fdab5f8f5bcc45d765eb548b3e051124ec8
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-24 10:25:16 +0000

    map function

--- 05_iterations_loops/seven.js
+++ 05_iterations_loops/seven.js
@@ -1,10 +1,13 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const akkya = []
+//complete task from forEach:
+// const akkya = []
+// myNumbers.forEach( (num) => {
+//     let add = num + 10
+//     akkya.push(add)
+// } )
+// console.log(akkya);
 
-myNumbers.forEach( (num) => {
-    let add = num + 10
-    akkya.push(add)
-} )
-
-console.log(akkya);
+//map method:-
+const newNums = myNumbers.map( function (num) {return num + 10} )
+console.log(newNums);
