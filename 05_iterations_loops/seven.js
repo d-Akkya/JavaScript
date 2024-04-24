b49cab94496212a34ded2e2c6046613fb18ae925
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-24 10:48:09 +0000

    chaining

--- 05_iterations_loops/seven.js
+++ 05_iterations_loops/seven.js
@@ -9,6 +9,11 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(akkya);
 
 //map method:-
-const newNums = myNumbers.map( function (num) {return num + 10} )
-console.log(newNums);
+// const newNums = myNumbers.map( function (num) {return num + 10} )
 //we can add conditions in map but it will petrate all values unlike filter whenever the condition value is true it will return those valuesm, map is different.
+
+//chaining:
+const newNums = myNumbers
+                .map( (num) => num*10 )
+                .map((num) => num + 1)
+console.log(newNums);
