56cfe1ca54042438cef4c6836f83035c2aca341e
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-24 10:31:45 +0000

    map method

--- 05_iterations_loops/seven.js
+++ 05_iterations_loops/seven.js
@@ -1,6 +1,6 @@
 const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-//complete task from forEach:
+//complete value 'return' task from forEach:
 // const akkya = []
 // myNumbers.forEach( (num) => {
 //     let add = num + 10
@@ -11,3 +11,4 @@ const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //map method:-
 const newNums = myNumbers.map( function (num) {return num + 10} )
 console.log(newNums);
+
