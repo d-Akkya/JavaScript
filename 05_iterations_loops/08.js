2f9721b1cec38ecf26ae6eeebdc44b568627ce77
Akhilesh Bamane <akhileshbamane26@gmail.com>
2024-04-25 09:34:04 +0000

    reduce() method/loop

--- 05_iterations_loops/08.js
+++ 05_iterations_loops/08.js
@@ -9,6 +9,9 @@ const myNums = [1, 2, 3]
 const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0 ) //arrow function
 
 console.log(myTotal);
+//In this method currentValue is values of array, in first loop accumulator don't know what to take value, so the value of initialValue is assigned to accumulator i.e. 0 + currentValue i.e. 1 = 1
+//In the second step, the sum of acc & currval is assigned to accumulator + the second currentValue i.e. 2
+//This is how the reduce() method works & returns the total value of an array very easily.
 
 //Realtime example of reduce:
 const shoppingCart = [
@@ -32,5 +35,5 @@ const shoppingCart = [
 
 const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0 )
 
-console.log(priceToPay);
+console.log(priceToPay);  //22996
 
