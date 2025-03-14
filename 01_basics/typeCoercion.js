7d13d54955275143b5e28567ee3ef92bc4dc7384
Akhilesh Bamane <akhileshbamane26@gmail.com>
2025-03-14 19:17:08 +0530

    type coercion in js

---
+++ 01_basics/typeCoercion.js
@@ -0,0 +1,5 @@
+console.log("1" + 1)    // 11 ('+' operator performs to operation addition and concatination, so the result varies on types of operand on which this operation will perform snf it's type will string)
+console.log(1 + 1)     // 2
+console.log("1" - 1)   // 0 (js will automatically converts the string into number if possible, whenever there's use of '-' because it performs only substraction)
+console.log("1" * 1)   // 1 (type is number)
+console.log("1" / 1)   // 1
